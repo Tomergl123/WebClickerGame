08b6307c232504cd10ad34912e6d769288db94dc
Tomer Gluzman <tgluzman2@gmail.com>
2025-10-02 12:46:38 -0400

    Added upgrade multiplier feature

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let score = 0
 let multiplier = 1
+let cost = 10
 let clicker = document.getElementById("clicker");
 let clicks = document.getElementById("clicks");
 let reset = document.getElementById("reset");
 let multi = document.getElementById("multi");
 let multiText = document.getElementById("multiText");
-
+let costText = document.getElementById("costText");
 
 
 // creating function to LISTEN for click
@@ -18,13 +19,27 @@ clicker.addEventListener("click", function(){
 reset.addEventListener("click", function(){
     score = 0;
     multiplier = 1
+    cost = 10
     clicks.textContent = score
     multiText.textContent = multiplier
+    costText.textContent = cost
+
 })
 
 //Multiplier button
 multi.addEventListener("click", function(){
-    multiplier = multiplier * 2
-    multiText.textContent = multiplier
+    if (score >= cost){
+        multiplier = multiplier * 2
+        score = score - cost
+        clicks.textContent = score
+        multiText.textContent = multiplier
+        cost = cost * 2.5
+        costText.textContent = cost
+
+
+    } else {
+        multiplier = multiplier * 1
+        multiText.textContent = "Not enough clicks!"
+    }
 })
 
